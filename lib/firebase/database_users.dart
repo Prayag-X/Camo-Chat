df1aadb6d2a280e601db020cbef19b8f025543cf
Akshit17 <akshit.chodhary@gmail.com>
2023-04-10 19:22:46 +0530

    Debugging lines added and TODO for further work created

--- lib/firebase/database_users.dart
+++ lib/firebase/database_users.dart
@@ -5,11 +5,45 @@ class DatabaseUsers {
   final collectionUsers = FirebaseFirestore.instance.collection('Users');
 
   Future<void> registerUser(String uid, String email, String userName) async {
-    await collectionUsers.doc(uid).set({'user_name': userName, 'email': email});
+    print('CollectionReference object: $collectionUsers');
+    if (userName.isEmpty) {
+      print('Empty userName provided');
+      return;
+    }
+    else
+    {
+      print('userName NOT empty');
+      print(userName);
+    }
+
+    try {
+      await collectionUsers.doc(uid).set({'user_name': userName, 'email': email});
+    } catch (e) {
+      print('Error registering user: $e');
+    }
   }
 
   Future<String> getUserName(String uid) async => await collectionUsers
       .doc(uid)
       .get()
       .then((DocumentSnapshot doc) => doc['user_name']);
+
+
+  // Join group
+
+  // Remove group
+
+  // Create group
+
+  // Groups list (realtime streambuilder)
+
+  // Dms list (realtime streambuilder)
+
+  // Open Dm
+
+  // Posts in group (realtime streambuilder)
+
+  // Dm in chatrooms (1on1) (realtime streambuilder)
+
+
 }
