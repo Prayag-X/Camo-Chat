b94c4ea99e95d8f647fe78ce968ca4a10c506d28
Akshit17 <akshit.chodhary@gmail.com>
2023-04-18 21:12:34 +0530

    Added like and report button

--- lib/pages/home_page/cards/message_card.dart
+++ lib/pages/home_page/cards/message_card.dart
@@ -55,71 +55,183 @@ class _MessageCardState extends State<MessageCard> {
     // controller.scrollDown();
   }
 
+  // Function to handle like button pressed
+  void onLikePressed() {
+    // TODO: Implement function to handle like button press
+  }
+
+  // Function to handle report button pressed
+  void onReportPressed() {
+    // TODO: Implement function to handle report button press
+  }
+
   @override
   Widget build(BuildContext context) {
     return !loading
-        ? !blank ? Center(
+        ? !blank
+        ? Center(
           child: Container(
             margin: const EdgeInsets.only(left: 10, right: 25, top: 5, bottom: 5),
             padding: EdgeInsets.only(
                 left: myMessage ? 60 : 0, right: myMessage ? 0 : 60),
             alignment:
-                myMessage ? Alignment.centerRight : Alignment.centerLeft,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: const Radius.circular(20.0),
-                  topLeft: const Radius.circular(20.0),
-                  bottomLeft: myMessage
-                      ? const Radius.circular(20.0)
-                      : const Radius.circular(0),
-                  bottomRight: myMessage
-                      ? const Radius.circular(0)
-                      : const Radius.circular(20),
-                ),
-                color: myMessage
-                    ? Themes.themeGroup.withOpacity(0.8)
-                    : Themes.themeDm.withOpacity(0.8),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            myMessage ? Alignment.centerRight : Alignment.centerLeft,
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 10),
+            Expanded(
               child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: myMessage ? CrossAxisAlignment.start : CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: myMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        myMessage ? "You" : message!.sender!.userName,
-                        style: const TextStyle(
-                            fontSize: 16,
-                            color: Themes.themeChatSender,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "${message!.timestamp.hour}:${message!.timestamp.minute}",
-                        style: TextStyle(
-                            fontSize: 9,
-                            color: Themes.themeChatTimer.withOpacity(0.7)),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 5,),
                   Text(
-                    message!.content,
-                    // maxLines: 3,
-                    // overflow: TextOverflow.ellipsis,
+                    myMessage ? "You" : message!.sender!.userName,
                     style: const TextStyle(
-                        fontSize: 13,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
+                        fontSize: 14,
+                        color: Themes.themeChatSender,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: const Radius.circular(15.0),
+                        topLeft: const Radius.circular(15.0),
+                        bottomLeft: myMessage
+                            ? const Radius.circular(20.0)
+                            : const Radius.circular(0),
+                        bottomRight: myMessage
+                            ? const Radius.circular(0)
+                            : const Radius.circular(20),
+                      ),
+                      color: myMessage
+                          ? Themes.themeGroup.withOpacity(0.8)
+                          : Themes.themeDm.withOpacity(0.8),
+                    ),
+                    // padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Column(
+                      crossAxisAlignment: myMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          message!.content,
+                          style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          width: 60+25,
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.thumb_up, size: 15),
+                                color: Colors.white,
+                                onPressed: () {
+                                  // TODO: Implement like functionality
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.report, size: 15),
+                                color: Colors.redAccent,
+                                onPressed: () {
+                                  // TODO: Implement report functionality
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "${message!.timestamp.hour}:${message!.timestamp.minute}",
+                    style: TextStyle(
+                        fontSize: 9,
+                        color: Themes.themeChatTimer.withOpacity(0.7)),
                   ),
                 ],
               ),
             ),
-          ),
-        ) : const SizedBox(height: 70,)
+            const SizedBox(width: 10),
+          ],
+      ),
+    ),
+        )
+        : const SizedBox(height: 70,)
         : Container();
   }
+
+
+
+
+// @override
+  // Widget build(BuildContext context) {
+  //   return !loading
+  //       ? !blank ? Center(
+  //         child: Container(
+  //           margin: const EdgeInsets.only(left: 10, right: 25, top: 5, bottom: 5),
+  //           padding: EdgeInsets.only(
+  //               left: myMessage ? 60 : 0, right: myMessage ? 0 : 60),
+  //           alignment:
+  //               myMessage ? Alignment.centerRight : Alignment.centerLeft,
+  //           child: Container(
+  //             decoration: BoxDecoration(
+  //               borderRadius: BorderRadius.only(
+  //                 topRight: const Radius.circular(20.0),
+  //                 topLeft: const Radius.circular(20.0),
+  //                 bottomLeft: myMessage
+  //                     ? const Radius.circular(20.0)
+  //                     : const Radius.circular(0),
+  //                 bottomRight: myMessage
+  //                     ? const Radius.circular(0)
+  //                     : const Radius.circular(20),
+  //               ),
+  //               color: myMessage
+  //                   ? Themes.themeGroup.withOpacity(0.8)
+  //                   : Themes.themeDm.withOpacity(0.8),
+  //             ),
+  //             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+  //             child: Column(
+  //               mainAxisSize: MainAxisSize.min,
+  //               crossAxisAlignment: myMessage ? CrossAxisAlignment.start : CrossAxisAlignment.start,
+  //               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //               children: [
+  //                 Column(
+  //                   crossAxisAlignment: CrossAxisAlignment.start,
+  //                   children: [
+  //                     Text(
+  //                       myMessage ? "You" : message!.sender!.userName,
+  //                       style: const TextStyle(
+  //                           fontSize: 16,
+  //                           color: Themes.themeChatSender,
+  //                           fontWeight: FontWeight.bold),
+  //                     ),
+  //                     Text(
+  //                       "${message!.timestamp.hour}:${message!.timestamp.minute}",
+  //                       style: TextStyle(
+  //                           fontSize: 9,
+  //                           color: Themes.themeChatTimer.withOpacity(0.7)),
+  //                     ),
+  //                   ],
+  //                 ),
+  //                 const SizedBox(height: 5,),
+  //                 Text(
+  //                   message!.content,
+  //                   // maxLines: 3,
+  //                   // overflow: TextOverflow.ellipsis,
+  //                   style: const TextStyle(
+  //                       fontSize: 13,
+  //                       color: Colors.white,
+  //                       fontWeight: FontWeight.w500),
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //         ),
+  //       ) : const SizedBox(height: 70,)
+  //       : Container();
+  // }
 }
